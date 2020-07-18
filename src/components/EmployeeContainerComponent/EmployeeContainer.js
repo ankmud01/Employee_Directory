dc9bb37ec70fdb3aec06f800c3cfb35ed04d70e2
Rebel Soul <mudvari.ankit@gmail.com>
2020-07-18 19:19:20 -0400

    added employee using the json file..

--- src/components/EmployeeContainerComponent/EmployeeContainer.js
+++ src/components/EmployeeContainerComponent/EmployeeContainer.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import Header from '../HeaderComponent/Header';
 import Search from '../SearchComponent/Search';
+import employees from '../../utils/employee.json';
 
 export default class EmployeeContainer extends Component {
   state = {
-    Picure: '1',
-    FirstName: 'Ankit',
-    LastName: 'Mudvari',
-    Email: 'ankmud01@gmail.com',
-    PhoneNumber: '120-456-7891',
-    Department: 'Information Technology',
-    Manager: 'John Kim',
+    // Picure: '1',
+    // FirstName: 'Ankit',
+    // LastName: 'Mudvari',
+    // Email: 'ankmud01@gmail.com',
+    // PhoneNumber: '120-456-7891',
+    // Department: 'Information Technology',
+    // Manager: 'John Kim',
+    employees,
   };
+
   render() {
     return (
       <div>
@@ -32,15 +35,17 @@ export default class EmployeeContainer extends Component {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>{this.state.Picure}</td>
-                <td>{this.state.FirstName}</td>
-                <td>{this.state.LastName}</td>
-                <td>{this.state.Email}</td>
-                <td>{this.state.PhoneNumber}</td>
-                <td>{this.state.Department}</td>
-                <td>{this.state.Manager}</td>
-              </tr>
+              {this.state.employees.map(employee => (
+                <tr>
+                  <td><img alt={employee.firstName} src = {employee.picture} /></td>
+                  <td>{employee.firstName}</td>
+                  <td>{employee.lastName}</td>
+                  <td>{employee.email}</td>
+                  <td>{employee.phone}</td>
+                  <td>{employee.department}</td>
+                  <td>{employee.manager}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
